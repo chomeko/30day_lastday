11e33bec0df5be5019ebe10d3e1db99758762be1
chomeko3o <kusyopi@gmail.com>
2020-02-01 19:37:56 +0900

    nwesのtext位置の修正とhover時のカラー変更

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,10 +3,23 @@ $( function() {
       speed: 400,
       autoplay:true,
       loop: true,
-      slidesPerView: 1,
+      centeredSlides: true,
+      slideToClickedSlide: true,
+      slidesPerView: 2,
+      spaceBetween: 40,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+      },
+      breakpoints: {
+        1180: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        767: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
       },
   } );
 } );
