eebf383c54ecff473067356b07ad53f8c6f8515e
chomeko3o <kusyopi@gmail.com>
2020-02-07 21:56:05 +0900

    headerのドロワーメニュー完成

---
+++ src/js/drower.js
@@ -0,0 +1,11 @@
+$('.menu-trigger').on('click',function(){
+  if($(this).hasClass('active')){
+    $(this).removeClass('active');
+    $('nav').removeClass('open');
+    $('.overlay').removeClass('open');
+  } else {
+    $(this).addClass('active');
+    $('nav').addClass('open');
+    $('.overlay').addClass('open');
+  }
+});
