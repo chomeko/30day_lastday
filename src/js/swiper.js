4d78d566430e94f30eba94be746cbcadc24c2e96
chomeko3o <kusyopi@gmail.com>
2020-02-08 22:05:45 +0900

    swiperとバグ修正

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,25 +1,34 @@
 $( function() {
   var swiper = new Swiper('.swiper__container', {
       speed: 400,
-      autoplay:true,
-      loop: true,
-      centeredSlides: true,
-      slideToClickedSlide: true,
-      slidesPerView: 2,
-      spaceBetween: 40,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
+      autoplay: true,
+      //centeredSlides: true,
+      slideToClickedSlide: true,
+      slidesPerView: 2.6,
+      loop: true,
+      loopedSlides: 6,//loopとセットで画像数
+      spaceBetween: 40,
       breakpoints: {
-        1180: {
+        1050: {
+          slidesPerView: 2.2,
+          spaceBetween: 40,
+        },
+        891: {
           slidesPerView: 2,
           spaceBetween: 40,
         },
         767: {
-          slidesPerView: 2,
+          slidesPerView: 1.27,
+          spaceBetween: 24,
+        },
+        374: {
+          slidesPerView: 1,
           spaceBetween: 24,
         },
       },
-  } );
-} );
+  });
+});
