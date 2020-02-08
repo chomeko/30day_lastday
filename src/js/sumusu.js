6b72221453cc72435fc2f4d59f9361f5521d43a4
chomeko3o <kusyopi@gmail.com>
2020-02-08 22:45:39 +0900

    微調整

--- src/js/sumusu.js
+++ src/js/sumusu.js
@@ -2,14 +2,14 @@ $(function () {
   /* aタグをクリックしたら*/
   $('a[href^="#"]').click(function () {
     /* ヘッダーの高さ*/
-    var adjust = -80;
+    var adjust = 68;
     var speed = 500;
     /*クリックしたaタグのhref属性の値を取得*/
     var href = $(this).attr("href");
     /*hrefが'#'または空白の場合'html'をそれ以外なら変数hrefを代入*/
     var target = $(href == "#" || href == "" ? 'html' : href);
     /*変数ターゲットの位置をoffset().topで上から何pxか取得*/
-    var position = target.offset().top + adjust;
+    var position = target.offset().top - adjust;
     /* html,bodyはブラウザ毎に違うから*/
     /* アニメーション（{scrollTopに変数positionの位置},数値ミリ秒）*/
     $("html, body").animate({ scrollTop: position }, speed, "swing");
